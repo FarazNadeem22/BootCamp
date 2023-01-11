c5c413a0f232b889fc5c21bb98843192f1d13017
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2023-01-10 23:22:13 -0500

    Create ORM.py

---
+++ ORM_vs_Raw/ORM.py
@@ -0,0 +1,71 @@
+import psycopg2
+
+# Connect to your POSTGRES DB
+conn = psycopg2.connect(
+    """
+    dbname=week3 user=postgres host=localhost port=5432
+    """
+)
+
+# Set autocommit to true 
+conn.set_session(autocommit=True)
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+cur.execute(
+    """
+    DROP TABLE IF EXISTS veggies
+    """
+)
+
+cur.execute(
+    """
+    CREATE TABLE veggies(
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        color TEXT NOT NULL
+    )
+    """
+)
+
+cur.execute(
+    """
+    INSERT INTO veggies VALUES 
+    (1, 'carrot', 'orange'),
+    (2, 'onion', 'yellow'),
+    (3, 'zucchini', 'green'),
+    (4, 'squash', 'yellow'),
+    (5, 'pepper', 'red'),
+    (6, 'onion', 'red')
+    """
+)
+
+# Execute a query
+cur.execute(
+    """
+    SELECT * FROM veggies
+    """
+)
+
+# Retrieve query results
+records = cur.fetchall()
+
+# print(records)
+
+cur.execute(
+    """
+    SELECT color, name FROM veggies
+    ORDER BY name, color
+    """
+)
+
+veggie_records = cur.fetchall()
+for i, v in enumerate(veggie_records):
+    print(str(i+1) + ".", v[0].capitalize(), v[1].capitalize())
+
+# for row in veggie_records:
+#     #print(row)
+#     for col in row:
+#         print(col, end=" ")
+#     print(" ")
